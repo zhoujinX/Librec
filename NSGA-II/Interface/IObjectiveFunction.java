c638c010eb075a269f5a4c3a94e9ebb7ccdebf69
zhoujinX <61581691+zhoujinX@users.noreply.github.com>
2020-04-17 23:05:32 +0800

    Add files via upload

---
+++ NSGA-II/Interface/IObjectiveFunction.java
@@ -0,0 +1,27 @@
+/*
+ * This repository / codebase is Open Source and free for use and rewrite.
+ */
+package io.onclave.nsga.ii.Interface;
+
+import io.onclave.nsga.ii.datastructure.Allele;
+import io.onclave.nsga.ii.datastructure.Chromosome;
+import io.onclave.nsga.ii.datastructure.ParetoObject;
+
+/**
+ * this is an interface that each objective function object that is created must implement.
+ * all the methods must be overridden.
+ * without implementing this interface, no objective function object can be plugged into the
+ * algorithm as the algorithm will not understand the objective function from a generic level.
+ * 
+ * @author  Debabrata Acharya <debabrata.acharya@icloud.com>
+ * @version 1.0
+ * @since   0.1
+ */
+public interface IObjectiveFunction {
+	public double objectiveFunction(double geneVaue);
+	public double objectiveFunction(Allele[] geneticCode);//新增加的
+	public double objectiveFunction(Chromosome chromosome);
+	public double objectiveFunction(ParetoObject paretoObject);
+    
+    public String getAxisTitle();
+}//在objectivefunction中均有实现
